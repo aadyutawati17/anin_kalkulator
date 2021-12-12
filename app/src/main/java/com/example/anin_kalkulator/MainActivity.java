84b6fd2a24801e34f9b807b9f6da334d619bd94e
Anindia Adyutawati Nugraheni <nugrahenigabriella@gmail.com>
2021-12-12 13:38:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/anin_kalkulator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.anin_kalkulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
